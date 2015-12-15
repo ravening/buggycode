39293727ed41ea752bbe561020a704cd8db3d739
Rakesh Venkatesh <rakeshv.com@gmail.com>
2015-12-14 23:26:06 -0800

    Climbing stairs
    program to find distinctive ways in which top can be reached

---
+++ git/src/climbStairs.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author rvenkatesh
+ *
+ * There are n steps and you can climb either 1 or 2 steps from each stair.
+ * In how many distinct ways we can reach the top
+ */
+public class climbStairs {
+	public static int numberofways(int n) {
+		if (n < 3) {
+			return n;
+		}
+		
+		int[] steps = new int[n];
+		steps[0] = 1;
+		steps[1] = 2;
+		
+		for (int i = 2; i < n; i++) {
+			steps[i] = steps[i-1] | steps[i-2];
+		}
+		
+		return steps[n-1];
+	}
+}
