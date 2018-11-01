ee72ed697f2a1fef87610d7e00b466ed638fd7e9
Rakesh Venkatesh <rakeshv.com@gmail.com>
2018-11-01 18:05:57 +0100

    Program to merge two lists in reverse order

---
+++ src/SingleLinkedList/MergeTwoListsReverse.java
@@ -0,0 +1,48 @@
+// given two linked lists, merge them in reverse order
+
+// Idea is same as merging two sorted linked lists. Instead of merging the nodes and moving right
+// we add the current smallest node at the beginning and move left
+package SingleLinkedList;
+
+public class MergeTwoListsReverse {
+    public Node mergeListsReverse(Node node1, Node node2) {
+        Node result = null;
+
+        if (node1 == null && node2 == null)
+            return null;
+
+        while (node1 != null && node2 != null) {
+            // get the smallest value and insert it at the beginning of the list
+            if (node1.data < node2.data) {
+                Node tmp = node1.next;
+                node1.next = result;
+                result = node1;
+                node1 = tmp;
+            }
+
+            if (node2.data < node1.data) {
+                Node tmp = node2.next;
+                node2.next = result;
+                result = node2;
+                node2 = tmp;
+            }
+        }
+
+        // if of the list is null, then just copy over the elements
+        while (node1 != null) {
+            Node tmp = node1.next;
+            node1.next = result;
+            result = node1;
+            node1 = tmp;
+        }
+
+        while (node2 != null) {
+            Node tmp = node2.next;
+            node2.next = result;
+            result = node2;
+            node2 = tmp;
+        }
+
+        return result;
+    }
+}
