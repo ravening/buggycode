91e20594de69c35853f227e9f238a2e94b6917be
Rakesh Venkatesh <rakeshv.com@gmail.com>
2018-11-03 10:46:19 +0100

    Function to arrange odd and even value nodes together

--- src/SingleLinkedList/RearrangeEvenOddPositions.java
+++ src/SingleLinkedList/RearrangeEvenOddPositions.java
@@ -31,4 +31,69 @@ public class RearrangeEvenOddPositions {
 
         return root;
     }
+
+    // Function to store all even values first followed by odd values
+    public void evenAndOddValuesTogether(Node head) {
+        if (head == null)
+            return;
+
+        Node current = head;
+        Node evenStart = null, evenEnd = null;
+        Node oddStart = null, oddEnd = null;
+
+        while (current != null) {
+            int val = current.data;
+            if (val % 2 == 0) {
+                // first node in the even list
+                if (evenStart == null) {
+                    evenEnd = current;
+                    evenStart = current;
+                } else {
+                    evenEnd.next = current;
+                    evenEnd = evenEnd.next;
+                }
+            } else {
+                // first node in the odd list
+                if (oddStart == null) {
+                    oddStart = current;
+                    oddEnd = current;
+                } else {
+                    oddEnd.next = current;
+                    oddEnd = oddEnd.next;
+                }
+            }
+            current = current.next;
+        }
+
+        // if both of the lists are null
+        if (evenStart == null && oddStart == null)
+            return;
+
+        // If odd numbers should appear after even numbers then append
+        // odd number list to end of even number list
+        evenEnd.next = oddStart;
+        oddEnd.next = null;
+        head = evenStart;
+
+        // If we want even and odd numbers alternatively.
+        // Now we have two list. merge them alternatively
+        Node result = null;
+        while (evenStart != null && oddStart != null) {
+            result = evenStart;
+            evenStart = evenStart.next;
+
+            result.next = oddStart;
+            oddStart = oddStart.next;
+
+            result = result.next;
+        }
+
+        if (evenStart != null) {
+            result.next = oddStart;
+        }
+
+        if (oddStart != null) {
+            result.next = evenStart;
+        }
+    }
 }
