0ae3b34ed49adf173328cb41c06c1555cb6644b7
r.venkatesh <rakeshv.com@gmail.com>
2019-03-01 10:16:05 +0100

    Program to search anagram substring in a string

---
+++ src/Trees/SumOfTreeNodes.java
@@ -0,0 +1,36 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SumOfTreeNodes {
+    ArrayList<Integer> queue = new ArrayList<>();
+    static int sum = 0;
+
+    public int solution(Node root) {
+        queue.add(root.val);
+
+        if (root.left != null) {
+            sum = solution(root.left);
+        }
+
+        if (root.right != null) {
+            sum = solution(root.right);
+        }
+
+        int t = 1;
+        int num = 0;
+
+        int size = queue.size();
+
+        while (size >= 0) {
+            num += queue.get(size-1) * t;
+            t = t * 10;
+        }
+
+        sum = sum + num;
+        queue.remove(size-1);
+        return  sum;
+    }
+}
