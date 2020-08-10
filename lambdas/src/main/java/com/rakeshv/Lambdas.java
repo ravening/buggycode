983d2f2c7c21f96c4dd43878b5f0792eb2ffa70a
Rakesh Venkatesh <rakeshv.com@gmail.com>
2020-08-10 12:44:45 +0200

    adding lambdas

---
+++ lambdas/src/main/java/com/rakeshv/Lambdas.java
@@ -0,0 +1,13 @@
+package com.rakeshv;
+
+/**
+ * Hello world!
+ *
+ */
+public class Lambdas
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
